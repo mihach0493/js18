0986ceb05fb0b9976c8a4d429a57c6cf6d3c3afd
Mykhailo Chui <mihach0493@gmail>
2020-09-15 19:47:49 +0200

    test

---
+++ main.js
@@ -0,0 +1,18 @@
+function dodajOdejmijMnoz(number1, number2) {
+    let suma = number1 + number2;
+    let roznica = number1 - number2;
+    let iloczyn = number1 * number2;
+
+    console.log("wynik dodawania wynosi " + suma);
+    console.log("wynik odejmowanie wynosi " + roznica);
+    console.log("wynik mnozenia wynosi " + iloczyn);
+
+}
+dodajOdejmijMnoz(10, 20);
+let liczba1 = 20;
+let liczba2 = 6;
+let liczba3 = 11;
+dodajOdejmijMnoz(liczba1, liczba2);
+
+dodajOdejmijMnoz(7, liczba3);
+dodajOdejmijMnoz(liczba2, liczba1);
