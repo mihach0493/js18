3464c3ea3bd80de5a08f6cea1c30a3da2a5b57f0
Mykhailo Chui <mihach0493@gmail>
2020-09-16 18:56:09 +0200

    test1

--- main.js
+++ main.js
@@ -14,5 +14,29 @@ let liczba2 = 6;
 let liczba3 = 11;
 dodajOdejmijMnoz(liczba1, liczba2);
 
-dodajOdejmijMnoz(7, liczba3);
+dodajOdejmijMnoz(liczba2, liczba3);
 dodajOdejmijMnoz(liczba2, liczba1);
+
+
+let suma = number1 + number2;
+if (suma > 0) {
+    console.log("wynik dodawania wynosi" + suma);
+} else {
+    console.log("Wynik jest nieprawidłowy");
+}
+
+let roznica = number1 - number2;
+if (suma > 0) {
+    console.log("wynik dodawania wynosi" - suma);
+} else {
+    console.log("Wynik jest nieprawidłowy");
+}
+
+let iloczyn = number1 * number2;
+if (suma > 0) {
+    console.log("wynik dodawania wynosi" * suma);
+} else {
+    console.log("Wynik jest nieprawidłowy");
+}
+
+console.log(dodajOdejmijMnoz(number1, number2));
