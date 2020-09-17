6d0335553f94243a27d34ad84c34ca5e715163cf
Mykhailo Chui <mihach0493@gmail>
2020-09-17 18:32:27 +0200

    zmiana2

--- main.js
+++ main.js
@@ -3,10 +3,30 @@ function dodajOdejmijMnoz(number1, number2) {
     let roznica = number1 - number2;
     let iloczyn = number1 * number2;
 
+
+    if (suma > 0) {
+        console.log("wynik dodawania wynosi" + suma);
+    } else {
+        console.log("Wynik jest nieprawidłowy");
+    }
+
+    if (suma > 0) {
+        console.log("wynik dodawania wynosi" - suma);
+    } else {
+        console.log("Wynik jest nieprawidłowy");
+    }
+
+
+    if (suma > 0) {
+        console.log("wynik dodawania wynosi" * suma);
+    } else {
+        console.log("Wynik jest nieprawidłowy");
+    }
+
     console.log("wynik dodawania wynosi " + suma);
     console.log("wynik odejmowanie wynosi " + roznica);
     console.log("wynik mnozenia wynosi " + iloczyn);
-
+    console.log(dodajOdejmijMnoz(number1, number2));
 }
 dodajOdejmijMnoz(10, 20);
 let liczba1 = 20;
@@ -18,25 +38,5 @@ dodajOdejmijMnoz(liczba2, liczba3);
 dodajOdejmijMnoz(liczba2, liczba1);
 
 
-let suma = number1 + number2;
-if (suma > 0) {
-    console.log("wynik dodawania wynosi" + suma);
-} else {
-    console.log("Wynik jest nieprawidłowy");
-}
 
-let roznica = number1 - number2;
-if (suma > 0) {
-    console.log("wynik dodawania wynosi" - suma);
-} else {
-    console.log("Wynik jest nieprawidłowy");
-}
 
-let iloczyn = number1 * number2;
-if (suma > 0) {
-    console.log("wynik dodawania wynosi" * suma);
-} else {
-    console.log("Wynik jest nieprawidłowy");
-}
-
-console.log(dodajOdejmijMnoz(number1, number2));
